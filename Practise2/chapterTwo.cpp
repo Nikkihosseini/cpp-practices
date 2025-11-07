6733bca37656908391c78f05396ad7e4ae139750
Nikkihosseini <niekal00@gmail.com>
2025-11-07 16:54:49 +0330

    feat: add examples for C++ variable initialization methods

---
+++ Practise2/chapterTwo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main()
+{
+    int a;       // (defult initialization)
+    int a = 7;   // (copy initialization)
+    int b(7);    // (direct initialization)
+    int c{7};    // (uniform initialization)
+
+    return 0;
+}
