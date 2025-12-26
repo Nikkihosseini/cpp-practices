9d05a13bfa8b98f18935cd7e4d5c09d0c44a01fa
Nikkihosseini <niekal00@gmail.com>
2025-12-26 18:39:10 +0330

    feat: add exercise 10 – for arrays

---
+++ Exam2/test10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int arry[4];
+
+    arry[0] = 1;
+    arry[1] = 2;
+    arry[2] = 3;
+    arry[3] = 4;
+
+    std::cout << "arry[0]" << " => " << arry[0] << '\n';
+    std::cout << "arry[1]" << " => " << arry[1] << '\n';
+    std::cout << "arry[2]" << " => " << arry[2] << '\n';
+    std::cout << "arry[3]" << " => " << arry[3] << '\n';
+
+    return 0;
+
+};
